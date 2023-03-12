546f7d22be5ee3f7587feba5d591bff9e0e06c7a
brucemckay <brucemckayone@gmail.com>
2023-03-12 13:35:25 +0000

    affiliate fix

--- pages/api/stripe/affiliate/links/onboarding.ts
+++ pages/api/stripe/affiliate/links/onboarding.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import stripe from "@/lib/stripe/stripe";
+import Stripe from "stripe";
 import homeUrl from "@/lib/constants/urls";
 export default async function handler(
   req: NextApiRequest,
@@ -10,8 +11,8 @@ export default async function handler(
     const accountId = req.query.accountId;
     const accountLink = await stripe.accountLinks.create({
       account: accountId as string,
-      refresh_url: `/affiliates/portal`,
-      return_url: `/api/affiliate/onboarding_return?accountId=${accountId}`,
+      refresh_url: `${homeUrl}/affiliates/portal`,
+      return_url: `${homeUrl}/api/affiliate/onboarding_return?accountId=${accountId}`,
       type: "account_onboarding",
     });
     res.status(200).json(accountLink);
