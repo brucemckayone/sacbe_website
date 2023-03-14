23aaf6649cb1a51b7602c1dab071a88d6928058a
brucemckay <brucemckayone@gmail.com>
2023-03-14 20:06:00 +0000

    affiliate fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -14,6 +14,26 @@ export default NextAuth({
   session: {
     strategy: "jwt",
   },
+  cookies: {
+    csrfToken: {
+      name: "next-auth.csrf-token",
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: true,
+      },
+    },
+    pkceCodeVerifier: {
+      name: "next-auth.pkce.code_verifier",
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: true,
+      },
+    },
+  },
   providers: [
     CredentialsProvider({
       name: "Email & Password",
