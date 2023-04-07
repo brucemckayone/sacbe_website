2fe5f43b3373bfdbb934fc75c8c581c8f19be872
brucemckay <brucemckayone@gmail.com>
2023-04-07 19:12:25 +0100

    removed need for customer account for subscriptions, also removed possiblity of duplicate orders

--- lib/constants/urls.ts
+++ lib/constants/urls.ts
@@ -1,5 +1,5 @@
 const homeUrl =
   process.env.NODE_ENV == "production"
-    ? "https://sacbe-ceremonial-cacao.vercel.app"
+    ? "https://sacbe-ceremonial-cacao.com"
     : "http://localhost:3000";
 export default homeUrl;
