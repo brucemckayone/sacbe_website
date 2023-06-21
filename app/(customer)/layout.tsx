acedbfca4d6f1881dde4fe3ce53a3b7969aa2d4e
brucemckay <brucemckayone@gmail.com>
2023-06-21 10:48:33 +0100

    tring

--- app/(customer)/layout.tsx
+++ app/(customer)/layout.tsx
@@ -103,6 +103,16 @@ export default function RootLayout({
     >
       <AuthProvider>
         <UserProvider>
+          <Head>
+            <Script id="gtm-script" strategy="afterInteractive">
+              {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-TWRTPQB');');
+              `}
+            </Script>
+          </Head>
           <body>
             <Navbar />
             <Suspense fallback={<PostLoader />}>
@@ -112,9 +122,18 @@ export default function RootLayout({
               </AffiliateLinkProvider>
               <Footer />
               <Toaster />
-              <GoogleAnalytics />
             </Suspense>
           </body>
+          <Suspense>
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=GTM-TWRTPQB`}
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              />
+            </noscript>
+          </Suspense>
         </UserProvider>
       </AuthProvider>
       <Analytics />
--- components/analytics/GoogleAnalytics.tsx
+++ components/analytics/GoogleAnalytics.tsx
@@ -11,19 +11,14 @@ export default function GoogleAnalytics() {
           style={{ display: "none", visibility: "hidden" }}
         />
       </noscript>
-      <Script
-        id="gtm-script"
-        strategy="afterInteractive"
-        async
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5PZP6JL');');
-  `,
-        }}
-      />
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5PZP6JL');');
+        `}
+      </Script>
     </>
   );
 }
