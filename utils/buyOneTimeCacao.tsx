f28ea37648540b6aa80c92b58c1f1f104357d6fd
brucemckay <brucemckayone@gmail.com>
2023-08-25 11:36:11 +0100

    pushing to preview

--- utils/buyOneTimeCacao.tsx
+++ utils/buyOneTimeCacao.tsx
@@ -5,11 +5,23 @@ export async function buySubscriptionFirstClass(
   user: userType,
   subQty: number
 ) {
+  const getPrices = () => {
+    const env = process.env.NODE_ENV;
+    if (env == "development") {
+      return [
+        "price_1NIqy6G859ZdyFmpzaNNkSNu",
+        "price_1NIsjZG859ZdyFmpvMG66qkf",
+      ];
+    } else {
+      return [
+        "price_1NLYCcG859ZdyFmpa95GIeSb",
+        "price_1NLYCaG859ZdyFmprZcXvCYg",
+      ];
+    }
+  };
+
   await createCheckoutSession({
-    prices: [
-      "price_1NLYCcG859ZdyFmpa95GIeSb",
-      "price_1NLYCTG859ZdyFmpJCrCATal",
-    ],
+    prices: getPrices(),
     mode: "subscription",
     customerId: user.customerId ?? undefined,
     qty: subQty,
@@ -20,11 +32,23 @@ export async function buySubscriptionSecondClass(
   user: userType,
   subQty: number
 ) {
+  const getPrices = () => {
+    const env = process.env.NODE_ENV;
+    if (env == "development") {
+      return [
+        "price_1NIqy6G859ZdyFmpzaNNkSNu",
+        "price_1NIsiYG859ZdyFmpLEjRmAAZ",
+      ];
+    } else {
+      return [
+        "price_1NLYCcG859ZdyFmpa95GIeSb",
+        "price_1NLYCaG859ZdyFmprZcXvCYg",
+      ];
+    }
+  };
+
   await createCheckoutSession({
-    prices: [
-      "price_1NLYCcG859ZdyFmpa95GIeSb",
-      "price_1NLYCaG859ZdyFmprZcXvCYg",
-    ],
+    prices: getPrices(),
     mode: "subscription",
     customerId: user.customerId ?? undefined,
     qty: subQty,
@@ -32,8 +56,17 @@ export async function buySubscriptionSecondClass(
 }
 
 export async function buyOneTimeCacao(user: userType, oneoffQty: number) {
+  const getPrices = () => {
+    const env = process.env.NODE_ENV;
+    if (env == "development") {
+      return ["price_1NIqy6G859ZdyFmpEbQLnA5q"];
+    } else {
+      return ["price_1NLYCcG859ZdyFmpgkHOXIUZ"];
+    }
+  };
+
   await createCheckoutSession({
-    prices: ["price_1NLYCcG859ZdyFmpgkHOXIUZ"],
+    prices: getPrices(),
     mode: "payment",
     customerId: user.customerId ?? undefined,
     qty: 1,
