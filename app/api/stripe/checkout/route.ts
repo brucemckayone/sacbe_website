b0f6811fbbe00185e8927f86d37804db64bb94f3
brucemckay <brucemckayone@gmail.com>
2023-12-30 21:15:25 +0000

    training page complete

--- app/api/stripe/checkout/route.ts
+++ app/api/stripe/checkout/route.ts
@@ -13,6 +13,8 @@ export async function GET(req: NextRequest) {
 export async function POST(req: NextRequest) {
   const helper = getHelper();
   const resp = await helper.createCustomCheckout((await req.json()) as any);
+  console.log(resp);
+
   return NextResponse.json(resp);
 }
 export async function DELETE(req: NextRequest) {
