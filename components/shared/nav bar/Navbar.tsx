3a51d5c586ee2f8b6ef6155b88911a476c17bbff
brucemckay <brucemckayone@gmail.com>
2025-04-08 19:07:09 +0100

    one more time

--- components/shared/nav bar/Navbar.tsx
+++ components/shared/nav bar/Navbar.tsx
@@ -28,13 +28,11 @@ export default function Navbar() {
           </Link>
           <LoginButton />
         </header>
-        {/* <div className="w-full bg-recommendedGreen text-onError relative z-50">
+        <div className="w-full bg-recommendedGreen text-onError relative z-50">
           <p className="pl-2 text-black text-ellipsis text-xs md:text-lg  ">
-            Please Note: We are currently taking a break, any orders place
-            between now and the 15th of march will be shipped on the 18th of
-            march. Thank you for your understanding.
+            Currently taking pre-orders for shipping: 25th of April.
           </p>
-        </div> */}
+        </div>
       </div>
     </SessionProvider>
   );
